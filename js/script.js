ae22246ec55a7600762d783e298257da061370da
Gabriel José Soto Ravanhan <gabrielravanhanjose@gmail.com>
2022-07-24 20:01:22 -0300

    JavaScript da página

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const pokemonName = document.querySelector('.pokemon-name');
+const pokemonNumber = document.querySelector('.pokemon-number');
+const pokemonImg = document.querySelector('.pokemon-img');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input-search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+    const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    if (apiResponse.status === 200) {
+        const data = await apiResponse.json();
+        return data;
+    }
+}
+
+const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
+    const data = await fetchPokemon(pokemon);
+
+    if (data) {
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImg.style.display = 'block';
+        pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value = '';
+
+        searchPokemon = data.id;
+    } else {
+        pokemonName.innerHTML = 'Not found :(';
+        pokemonNumber.innerHTML = '';
+        pokemonImg.style.display = 'none';
+    }
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+});
+
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    }
+});
+
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
